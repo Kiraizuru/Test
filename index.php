e28c74d75d0d357d23f7cc437636c4779101786a
user <user@WIN-73MOPIH9IH1>
2024-04-23 10:35:04 +0500

    Добавьте файлы проекта.

---
+++ index.php
@@ -0,0 +1,13 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+</head>
+<body>
+<?php
+$str = "Hi";
+$islol = false;
+if ($str != "Hi" && !$islol);
+    echo "work"
+?>
+</body>
+</html>
