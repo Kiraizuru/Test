8fae663a6ba5aec4d132767fcc652420fe2ce9cf
Noserbaev Ibragim <draga963@gmail.com>
2024-04-25 12:03:10 +0500

    upd 1

--- index.php
+++ index.php
@@ -4,10 +4,28 @@
 </head>
 <body>
 <?php
-$str = "Hi";
-$islol = false;
-if ($str != "Hi" && !$islol);
-    echo "work"
+$servername = "localhost";
+$username = "root";
+$password = "415263Darw";
+$database = "Test";
+// Create connection
+$conn = new mysqli($servername, $username, $password, );
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli("$servername", "$username", "$password", "$database");
+$mysqli->query("DROP TABLE IF EXISTS test");
+$mysqli->query("CREATE TABLE test(id INT, label CHAR(1))");
+$mysqli->query("INSERT INTO test(id, label) VALUES (1, 'a')");
+;
+$result = $mysqli->query("SELECT id, label FROM test WHERE id = 1");
+$row = $result->fetch_assoc();
+printf("id = %s (%s)\n", $row['id'], gettype($row['id']));
+printf("label = %s (%s)\n", $row['label'], gettype($row['label']));
+// Check
+if ($conn->connect_error) {
+    die("" . $conn->connect_error);
+    echo ("Yes");
+} else
+    echo ("CONNECTION Ok");
 ?>
 </body>
 </html>
